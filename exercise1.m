310cc03f38beb1496d1eb4dfe07ad20f61a680e3
BamseL <43402436+BamseL@users.noreply.github.com>
2022-07-15 15:48:08 +0800

    Add files via upload

--- exercise1.m
+++ exercise1.m
@@ -1,52 +1,126 @@
 clc,clear;
 close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-f0=zeros(256,256);%Produce original image.
-[height,width]=size(f0);
-f0(25:234,[25:31,49:55,73:79,97:103,...
-    121:127,145:151,169:175,193:199,217:223])=255*ones(210,63);
-f=double(uint8(f0));
-figure,subplot(2,2,1),imshow(uint8(f)),title('Original image.');
+f=imread('lena512.jpg');
+[height,width]=size(f);
+a=min(min(f));%The minimum gray value of image.
+b=max(max(f));%The maximum gray value of image.
+x=unique(f);
+y=zeros(size(x));
+for i=1:length(x)%The frequency of the corresponding gray value.
+    y(i)=length(find(f==x(i)));
+end
+frequ=y./(height*width);
+%sum(frequ)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-f_d_r33=[f(:,1:4),f,f(:,width-3:width)];
-f_d_r3=[f_d_r33(1:4,:);f_d_r33;f_d_r33(height-3:height,:)];
-N3=3;
-f_L3=f_d_r3;
-for i=1:height
-    for j=1:width
-        s3=sum(sum(f_d_r3(i:i+2,j:j+2)));
-        f_L3(i+(N3-1)/2,j+(N3-1)/2)=s3/(N3*N3);
+figure,subplot(1,2,1),imshow(f),title('原始图像')%Original image.
+subplot(1,2,2),bar(x,frequ),title('频率直方图')%Frequency histogram.
+Hs=-(sum(frequ.*log2(frequ)));%Entropy.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fre_sort=sort(frequ,'descend');
+[m,n]=size(x);
+x_sort=zeros(m,n);
+len_t=1;
+for i=1:m
+    if len_t==1
+        len_t=length(find(frequ==fre_sort(i)));
+        x_sort(i:i+len_t-1)=x(frequ==fre_sort(i));
+    else
+        len_t=len_t-1;
+        continue;
     end
 end
-f_Last3=f_L3(2:height+1,2:width+1);
-f_L_u3=uint8(f_Last3);%8 bit image data.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-f_d_r55=[f(:,1:4),f,f(:,width-3:width)];
-f_d_r5=[f_d_r55(1:4,:);f_d_r55;f_d_r55(height-3:height,:)];
-N5=5;
-f_L5=f_d_r5;
-for i=1:height
-    for j=1:width
-        s5=sum(sum(f_d_r5(i:i+4,j:j+4)));
-        f_L5(i+(N5-1)/2,j+(N5-1)/2)=s5/(N5*N5);
+B=zeros(m,m);%Encoder matrix.
+for i=1:m
+    B(:,i)=fre_sort;%Initialize the first column of encoder matrix.
+end
+temp_fre_sort=fre_sort;
+for i=2:m%Encoder matrix
+    if i==2
+        temp_s=B(end-1,i-1)+B(end,i-1);
+        B(end-1,i)=temp_s;
+        B(end,i:end)=0;
+        temp_fre_sort=sort(B(:,i),'descend');
+        B(:,i)=temp_fre_sort;
+        B(:,i+1)=temp_fre_sort;
+        temp_addr=find(temp_fre_sort==temp_s,m);
+        B(end,i)=temp_addr(end);
+    else
+        temp_s=B(end-i+1,i-1)+B(end-i+2,i-1);
+        B(end-i+1,i)=temp_s;
+        B(end-i+2,i:end)=0;
+        temp_fre_sort=sort([B(1:end-1,i);0],'descend');
+        if i==m
+            B(:,i)=temp_fre_sort;
+        else
+            B(:,i)=temp_fre_sort;
+            B(:,i+1)=temp_fre_sort;
+        end
+        temp_addr=find(temp_fre_sort==temp_s,m);
+        B(end,i)=temp_addr(end);
     end
 end
-f_Last5=f_L5(3:height+2,3:width+2);
-f_L_u5=uint8(f_Last5);%8 bit image data.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-f_d_r99=[f(:,1:4),f,f(:,width-3:width)];
-f_d_r9=[f_d_r99(1:4,:);f_d_r99;f_d_r99(height-3:height,:)];
-N9=9;
-f_L9=f_d_r9;
-for i=1:height
-    for j=1:width
-        s9=sum(sum(f_d_r9(i:i+8,j:j+8)));
-        f_L9(i+(N9-1)/2,j+(N9-1)/2)=s9/(N9*N9);
+H_code=char(zeros(m,m));%The initial coding.
+for i=m:-1:2%Produce Hoffman coding.
+    if i==m
+        H_code(1,1)='0';
+        H_code(2,1)='1';
+    else
+        temp_H_code=H_code;
+        K=B(end,i);
+        for j=1:m-i%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            if j>1&&B(j,i-1)==B(j-1,i-1)%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                continue;
+            else
+                B(K,i)=-1;
+                temp_fre=B(:,i);
+                temp_addr=find(temp_fre==B(j,i-1));
+                if length(temp_addr)==1
+                    H_code(j,:)=temp_H_code(temp_addr,:);
+                else
+                    temp_L=length(temp_addr);
+                    H_code(j:j+temp_L-1,:)=temp_H_code(temp_addr,:);
+                end
+            end
+        end
+        code_length=length(find(temp_H_code(K,:)=='1'))+...
+            length(find(temp_H_code(K,:)=='0'));
+        H_code(m-i+1,1:code_length+1)...
+            =[temp_H_code(K,1:code_length),'0'];
+        H_code(m-i+2,1:code_length+1)...
+            =[temp_H_code(K,1:code_length),'1'];
     end
 end
-f_Last9=f_L9(5:height+4,5:width+4);
-f_L_u9=uint8(f_Last9);%8 bit image data.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-subplot(2,2,2),imshow(f_L_u3),title('3*3');
-subplot(2,2,3),imshow(f_L_u5),title('5*5');
-subplot(2,2,4),imshow(f_L_u9),title('9*9');
+len_average=0;
+for i=1:m
+    len_subcode=length(find(H_code(i,:)=='1'))+...
+        length(find(H_code(i,:)=='0'));
+    len_average=len_average+len_subcode*fre_sort(i);
+end
+[num2str(x_sort),char(ones(m,n).*32),char(ones(m,n).*32),H_code]
+Hs
+len_average
+y=Hs/len_average
+C=8/len_average
+R=1-y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
